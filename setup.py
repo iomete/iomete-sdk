64389f1190150f8f6b56fd33256e55ca38ab1db8
abhishekpathania <abhishek@iomete.com>
2024-12-03 09:57:21 +0530

    Drop support for older python version 3.7

--- setup.py
+++ setup.py
@@ -36,12 +36,11 @@ setup(
         "Operating System :: Microsoft :: Windows",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX :: Linux",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.8",
 )
